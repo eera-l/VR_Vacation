f5e34f861f53a8270c4ad4e5cae741278eb4553c
Federica <federica.comuni@gmail.com>
2019-01-07 12:26:16 +0100

    Redoing of the hibernate files

---
+++ VR_VacationWeb/src/java/hibernate/DBHelper.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Federica
+ */
+public class DBHelper {
+    
+    Session session = null;
+
+    public DBHelper() {
+        this.session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    
+    
+    
+}
