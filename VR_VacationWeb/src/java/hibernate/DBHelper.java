9bc961b22d365588973d72dc39c79aeb4c1df6ac
Federica <federica.comuni@gmail.com>
2019-01-09 20:29:43 +0100

    Added READ methods in db connector

--- VR_VacationWeb/src/java/hibernate/DBHelper.java
+++ VR_VacationWeb/src/java/hibernate/DBHelper.java
@@ -6,6 +6,9 @@
 package hibernate;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.Query;
+import java.util.List;
 
 /**
  *
@@ -19,6 +22,133 @@ public class DBHelper {
         this.session = HibernateUtil.getSessionFactory().getCurrentSession();
     }
     
+    public User findUserByEmail(String email) {
+        User user = null;
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from User as user where user.emailAddress = '" + email +
+                    "'");
+            user = (User)q.uniqueResult();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return user;
+    }
+    
+    public List<Order> findOrdersByUserEmail(String email) {
+        List<Order> orders = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Order as ord where ord.user.emailAddress = '" +
+                    email + "'");
+
+            orders = (List<Order>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return orders;
+    }
+    
+     public List<Package> findAllPackages() {
+
+        List<Package> packages = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Package as package");
+
+            packages = (List<Package>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return packages;
+    }
+     
+     
+    public List<Destination> findPackagesByOrder(int orderId) {
+        
+        List<Destination> dests = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Package as package where package.order.orderId = " + orderId);
+
+            dests = (List<Destination>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return dests;
+    }
+    
+    public List<Package> findPackagesByDestination(int destId) {
+         List<Package> packages = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Package as package where package.destination.destinationId = " + destId);
+
+            packages = (List<Package>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return packages;
+    }
+    
+    public List<Destination> findAllDestinations() {
+
+        List<Destination> dests = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Destination as destination");
+
+            dests = (List<Destination>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return dests;
+    }
+    
+    public List<Experience> findExpByDestination(int destId) {
+        
+        List<Experience> exps = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Experience as experience where experience.destination.destinationId = " + destId);
+
+            exps = (List<Experience>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return exps;
+        
+    }
+    
+    public List<Experience> findExpByPackage(int packageId) {
+        
+        List<Experience> exps = null;
+
+        try {
+            session.beginTransaction();
+            Query q = session.createQuery("from Experience as experience where experience.package.packageId = " + packageId);
+
+            exps = (List<Experience>)q.list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return exps;
+        
+    }
     
     
 }
