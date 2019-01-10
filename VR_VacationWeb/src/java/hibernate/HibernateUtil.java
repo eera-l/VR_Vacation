3de92b3ccaaf6411778762fd0fb11566417019f6
Federica <federica.comuni@gmail.com>
2019-01-10 22:31:49 +0100

    Added DBHelper functionalities

--- VR_VacationWeb/src/java/hibernate/HibernateUtil.java
+++ VR_VacationWeb/src/java/hibernate/HibernateUtil.java
@@ -1,12 +1,16 @@
+package hibernate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hibernate;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -16,13 +20,19 @@ import org.hibernate.SessionFactory;
  */
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory = buildSessionFactory();
     
-    static {
+    private static SessionFactory buildSessionFactory() {
+        
+        
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate/hibernate.cfg.xml");
+
+            StandardServiceRegistryBuilder serviceBuilder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+            return configuration.buildSessionFactory(serviceBuilder.build());
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
@@ -33,4 +43,10 @@ public class HibernateUtil {
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+    
+     public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+    
 }
