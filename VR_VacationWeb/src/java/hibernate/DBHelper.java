ed746a70319c73d10cde47c86ac6aeaf7c19bb6a
Federica Comuni <federica.comuni@gmail.com>
2019-01-30 19:20:26 +0100

    Update DBHelper.java

--- VR_VacationWeb/src/java/hibernate/DBHelper.java
+++ VR_VacationWeb/src/java/hibernate/DBHelper.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * DBHelper.java
+ * Funct.: it retrieves, updates and adds information
+ * on the database.
  */
 package hibernate;
 
