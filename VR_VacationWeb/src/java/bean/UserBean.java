be6011571deba4a1185b1b9e8e05b2973d853ed8
Federica Comuni <federica.comuni@gmail.com>
2019-01-31 18:50:09 +0100

    Update UserBean.java

--- VR_VacationWeb/src/java/bean/UserBean.java
+++ VR_VacationWeb/src/java/bean/UserBean.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * UserBean.java
+ * Handles retrieving of user's personal information,
+ * plus logging out and checking if the user is logged in
+ * functionalities.
  */
 package bean;
 
@@ -20,8 +21,7 @@ import javax.ejb.Stateless;
 @Stateless
 public class UserBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+   
     public boolean checkIfUserLoggedIn() {
 
         if (DataStorage.getInstance().getUser() != null) {
