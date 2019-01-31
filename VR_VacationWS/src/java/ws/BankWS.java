9057c41c9ac669dc990d823bfef8e975323aa8f6
Federica Comuni <federica.comuni@gmail.com>
2019-01-31 18:35:54 +0100

    Update BankWS.java

--- VR_VacationWS/src/java/ws/BankWS.java
+++ VR_VacationWS/src/java/ws/BankWS.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * BankWS.java
+ * Funct.: Web service that checks whether the credit card number matches
+ * the hard-coded ones and returns the result.
  */
 package ws;
 
