59842366b0df7ca92ad442682566c8f076e0de4e
Federica Comuni <federica.comuni@gmail.com>
2019-01-31 19:01:25 +0100

    Update ShoppingCart.java

--- VR_VacationWeb/src/java/global/ShoppingCart.java
+++ VR_VacationWeb/src/java/global/ShoppingCart.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ShoppingCart.java
+ * Persistence class for the shopping cart. 
+ * Its methods are called by the ShoppingCartBean.
  */
 package global;
 
