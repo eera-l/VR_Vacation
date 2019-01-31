86e5f2eba1410d53032b12c7a105a7eedda981cc
Federica Comuni <federica.comuni@gmail.com>
2019-01-31 18:45:19 +0100

    Update BankAppBean.java

--- VR_VacationWeb/src/java/bean/BankAppBean.java
+++ VR_VacationWeb/src/java/bean/BankAppBean.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Contacts the Bank Web Service to verify
+ * whether the customer's credit card number is
+ * approved for the purchase
  */
 package bean;
 
