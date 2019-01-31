63120b3fc0a8a97cd5a605713d15e40ce5aa69bc
Federica Comuni <federica.comuni@gmail.com>
2019-01-31 18:37:01 +0100

    Update TravelAgencyBean.java

--- VR_VacationJMS/src/java/bean/TravelAgencyBean.java
+++ VR_VacationJMS/src/java/bean/TravelAgencyBean.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * TravelAgencyBean.java
+ * Funct.: Message-driven bean which listens to messages arriving
+ * in the VRQueue
  */
 package bean;
 
